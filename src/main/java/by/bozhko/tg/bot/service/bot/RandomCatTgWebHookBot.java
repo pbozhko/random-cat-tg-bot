b8335f62f318ae8ed22496b81e1d1914c00e3148
Pavel Bozhko <p.bozhko92@mail.ru>
2020-10-18 17:58:26 +0300

    Add database support for images

--- src/main/java/by/bozhko/tg/bot/service/bot/RandomCatTgWebHookBot.java
+++ src/main/java/by/bozhko/tg/bot/service/bot/RandomCatTgWebHookBot.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -69,13 +70,14 @@ public class RandomCatTgWebHookBot extends TelegramWebhookBot {
 
             final List<String> allowedMimeTypes = List.of("image/bmp", "image/gif", "image/jpeg", "image/png");
 
-            PhotoSize photoSize = update.getMessage().getPhoto().get(1);
+            List<PhotoSize> allPhotoSizes = update.getMessage().getPhoto();
+
+            PhotoSize photoSize = allPhotoSizes.get(allPhotoSizes.size() - 1);
 
             try {
-                log.info("FilePath {}", photoSize.getFilePath());
-                log.info("FileId {}", photoSize.getFileId());
-
-                File tmpFile = downloadFile(photoSize.getFilePath());
+                GetFile getFile = new GetFile().setFileId(photoSize.getFileId());
+                String filePath = execute(getFile).getFilePath();
+                File tmpFile = downloadFile(filePath);
 
                 String mimeType = Files.probeContentType(tmpFile.toPath());
 
