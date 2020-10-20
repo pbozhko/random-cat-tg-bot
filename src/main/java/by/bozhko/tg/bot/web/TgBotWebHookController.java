bdaee10b885e67b4de57fc3b403ee7f6470ff161
Pavel Bozhko <p.bozhko92@mail.ru>
2020-10-20 17:15:31 +0300

    Use inline buttons

--- src/main/java/by/bozhko/tg/bot/web/TgBotWebHookController.java
+++ src/main/java/by/bozhko/tg/bot/web/TgBotWebHookController.java
@@ -4,11 +4,14 @@ import by.bozhko.tg.bot.dao.model.Account;
 import by.bozhko.tg.bot.dao.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 
@@ -23,33 +26,48 @@ public class TgBotWebHookController {
     private final AccountRepository accountRepository;
 
     @PostMapping("/api/v1/cat")
-    public final BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+    public final ResponseEntity<BotApiMethod<?>> onUpdateReceived(@RequestBody Update update) {
 
         log.info("New telegram request: {}", update);
 
-        User user = update.getMessage().getFrom();
+        Message message = update.getMessage();
 
-        Optional<Account> account = accountRepository.findByAccountId(user.getId().longValue());
+        if (message == null) {
 
-        if (account.isPresent()) {
-
-            log.info("Account is already known");
-        } else {
-
-            Account newAccount = new Account(
-                null,
-                user.getId().longValue(),
-                user.getFirstName(),
-                user.getLastName(),
-                user.getBot(),
-                user.getUserName(),
-                user.getLanguageCode()
-            );
-
-            log.info("Account is unknown. Create new one");
-            accountRepository.save(newAccount);
+            message = update.getCallbackQuery().getMessage();
         }
 
-        return randomCatTgWebHookBot.onWebhookUpdateReceived(update);
+        if (message != null) {
+
+            User user = update.getMessage().getFrom();
+
+            Optional<Account> account = accountRepository.findByAccountId(user.getId().longValue());
+
+            if (account.isPresent()) {
+
+                log.info("Account is already known");
+            } else {
+
+                Account newAccount = new Account(
+                    null,
+                    user.getId().longValue(),
+                    user.getFirstName(),
+                    user.getLastName(),
+                    user.getBot(),
+                    user.getUserName(),
+                    user.getLanguageCode()
+                );
+
+                log.info("Account is unknown. Create new one");
+                accountRepository.save(newAccount);
+            }
+
+            return new ResponseEntity<BotApiMethod<?>>(
+                randomCatTgWebHookBot.onWebhookUpdateReceived(update),
+                HttpStatus.OK
+            );
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
