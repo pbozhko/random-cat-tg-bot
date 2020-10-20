00c04931d16511598d40764107b3d1661f0b0132
Pavel Bozhko <p.bozhko92@mail.ru>
2020-10-20 15:46:06 +0300

    Add logging for all tg requests

---
+++ src/main/java/by/bozhko/tg/bot/dao/model/Account.java
@@ -0,0 +1,23 @@
+// package by.bozhko.tg.bot.dao.model;
+//
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+//
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
+//
+// @Data
+// @Entity
+// @Table(name = "t_accounts")
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class Account {
+//
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+// }
--- src/main/java/by/bozhko/tg/bot/web/TgBotWebHookController.java
+++ src/main/java/by/bozhko/tg/bot/web/TgBotWebHookController.java
@@ -1,6 +1,7 @@
 package by.bozhko.tg.bot.web;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class TgBotWebHookController {
 
     private final TelegramWebhookBot randomCatTgWebHookBot;
@@ -17,6 +19,7 @@ public class TgBotWebHookController {
     @PostMapping("/api/v1/cat")
     public final BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
 
+        log.info("New telegram request: {}", update);
         return randomCatTgWebHookBot.onWebhookUpdateReceived(update);
     }
 }
