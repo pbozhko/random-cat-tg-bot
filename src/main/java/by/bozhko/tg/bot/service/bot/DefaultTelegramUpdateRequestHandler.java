f8d2726d66ded5c5b0bd7dec7151a93bfd4e07b4
Pavel Bozhko <p.bozhko92@mail.ru>
2020-10-20 16:29:10 +0300

    Test inline keyboard

--- src/main/java/by/bozhko/tg/bot/service/bot/DefaultTelegramUpdateRequestHandler.java
+++ src/main/java/by/bozhko/tg/bot/service/bot/DefaultTelegramUpdateRequestHandler.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.io.ByteArrayInputStream;
@@ -78,20 +80,33 @@ public class DefaultTelegramUpdateRequestHandler implements TelegramUpdateReques
     @Override
     public SendMessage getFirstMessage(Update update) {
 
-        KeyboardRow keyboardRow = new KeyboardRow();
-        keyboardRow.add("Хочу Кита!");
-        keyboardRow.add("Хочу других котиков!");
+        // KeyboardRow keyboardRow = new KeyboardRow();
+        // keyboardRow.add("Хочу Кита!");
+        // keyboardRow.add("Хочу других котиков!");
+        //
+        // List<KeyboardRow> keyboard = List.of(keyboardRow);
+        //
+        // ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        // replyKeyboardMarkup.setOneTimeKeyboard(true);
+        // replyKeyboardMarkup.setResizeKeyboard(true);
+        // replyKeyboardMarkup.setKeyboard(keyboard);
 
-        List<KeyboardRow> keyboard = List.of(keyboardRow);
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("Хочу Кита!");
+        button1.setCallbackData("Хочу Кита!");
 
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        replyKeyboardMarkup.setOneTimeKeyboard(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setKeyboard(keyboard);
+        InlineKeyboardButton button2 = new InlineKeyboardButton();
+        button2.setText("Хочу других котиков!");
+        button2.setCallbackData("Хочу других котиков!");
+
+        List<InlineKeyboardButton> buttonsRow = List.of(button1, button2);
+        List<List<InlineKeyboardButton>> keyboard = List.of(buttonsRow);
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        inlineKeyboardMarkup.setKeyboard(keyboard);
 
         return new SendMessage()
             .setChatId(update.getMessage().getChatId())
             .setText("Привет! У меня есть много котиков. Хочешь их посмотреть?")
-            .setReplyMarkup(replyKeyboardMarkup);
+            .setReplyMarkup(inlineKeyboardMarkup);
     }
 }
