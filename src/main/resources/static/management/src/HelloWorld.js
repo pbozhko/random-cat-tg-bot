7883d2f85616f1ac573d4cfdf8a40114dde247a1
Pavel Bozhko <p.bozhko92@mail.ru>
2020-10-21 17:10:54 +0300

    Add images list on UI

--- src/main/resources/static/management/src/HelloWorld.js
+++ src/main/resources/static/management/src/HelloWorld.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Col, Container, Image, Nav, Navbar, Row} from 'react-bootstrap';
+import {Card, Col, Container, Nav, Navbar, Row} from 'react-bootstrap';
 import axios from 'axios';
 
 const HelloWorld = () => {
@@ -9,27 +9,39 @@ const HelloWorld = () => {
     useEffect(() => {
 
         const fetchData = async () => {
-            const response = await axios.get('/api/management/v1/photos').then(response => setData(response.data)).catch(error=>{});
+            const response = await axios.get('https://random-cat-tg-bot.herokuapp.com/api/management/v1/photos').then(response => setData(response.data)).catch(error => {
+            });
         }
         fetchData();
     }, []);
 
     return (
         <Container>
-            <Container>
-                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#accounts">Accounts</Nav.Link>
-                            <Nav.Link href="#photos">Photos</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </Container>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#accounts">Accounts</Nav.Link>
+                        <Nav.Link href="#photos">Photos</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
             <Row>
                 {
-                    allPhotos.map(photo => <Col md={4}> <Image fluid src={photo.url}/></Col>)
+                    allPhotos.map(photo =>
+                        <Col md={4}>
+                            <Card>
+                                <Card.Img variant="top" src={photo.url} />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
                 }
             </Row>
         </Container>
